58d1bb2bfb6548811a663164f7d2b29a1f428686
nikssan123 <nikssan123@gmail.com>
2021-09-18 02:17:51 +0300

    Adding more functionality

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
+const { MessageEmbed } = require("discord.js");
 require("dotenv").config();
-const { prefix, token } = require("./config.json");
+const { prefix } = require("./config.json");
 const ytdl = require("ytdl-core");
 const searchPlaylist = require("youtube-search-api");
 
@@ -31,11 +32,29 @@ client.on("message", async message => {
     if (message.content.startsWith(`${prefix}play`)) {
         execute(message, serverQueue);
         return;
+    } else if (
+        message.content.startsWith(`${prefix}skipTo`) ||
+        message.content.startsWith(`${prefix}skipto`)
+    ) {
+        skipTo(message, serverQueue);
+        return;
     } else if (message.content.startsWith(`${prefix}skip`)) {
         skip(message, serverQueue);
         return;
-    } else if (message.content.startsWith(`${prefix}stop`)) {
+    } else if (
+        message.content.startsWith(`${prefix}stop`) ||
+        message.content.startsWith(`${prefix}disconnect`)
+    ) {
         stop(message, serverQueue);
+        return;
+    } else if (message.content.startsWith(`${prefix}shuffle`)) {
+        shuffle(message, serverQueue);
+        return;
+    } else if (
+        message.content.startsWith(`${prefix}queue`) ||
+        message.content.startsWith(`${prefix}playlist`)
+    ) {
+        status(message, serverQueue);
         return;
     } else {
         message.channel.send("You need to enter a valid command!");
@@ -43,6 +62,7 @@ client.on("message", async message => {
 });
 
 const queue = new Map();
+let playMsg;
 
 async function execute(message, serverQueue) {
     const args = message.content.split(" ");
@@ -72,6 +92,8 @@ async function execute(message, serverQueue) {
             songs = playlist.items.map(newSong => ({
                 title: newSong.title,
                 url: "https://www.youtube.com/watch?v=" + newSong.id,
+                author: "",
+                image: newSong.thumbnail.thumbnails[0].url,
             }));
         } catch (e) {
             console.log("NOT a Playlist");
@@ -83,6 +105,8 @@ async function execute(message, serverQueue) {
             song = {
                 title: songInfo.videoDetails.title,
                 url: songInfo.videoDetails.video_url,
+                author: songInfo.videoDetails.author.name,
+                image: songInfo.videoDetails.thumbnails[0].url,
             };
         } catch (e) {
             console.log("Not URL");
@@ -100,6 +124,8 @@ async function execute(message, serverQueue) {
             song = {
                 title: results[0].title,
                 url: results[0].link,
+                author: results[0].channelTitle,
+                image: results[0].thumbnails.default.url,
             };
         }
     }
@@ -129,15 +155,33 @@ async function execute(message, serverQueue) {
         }
     } else {
         serverQueue.songs.push(song);
-        return message.channel.send(`${song.title} has been added to the queue!`);
+        return message.channel.send(`**${song.title}** has been added to the queue!`);
     }
 }
 
 function skip(message, serverQueue) {
     if (!message.member.voice.channel)
-        return message.channel.send("You have to be in a voice channel to stop the music!");
+        return message.channel.send("You have to be in a voice channel to skip!");
     if (!serverQueue) return message.channel.send("There is no song that I could skip!");
     serverQueue.connection.dispatcher.end();
+}
+
+function skipTo(message, serverQueue) {
+    if (!message.member.voice.channel)
+        return message.channel.send("You have to be in a voice channel to skip!");
+    if (!serverQueue) return message.channel.send("There is no song that I could skip!");
+
+    const skips = message.content.split(" ")[1];
+
+    console.log("in skip to");
+
+    if (isNumeric(skips)) {
+        // const currentSongs = serverQueue.songs.slice(0, 10);
+        serverQueue.songs.splice(0, skips);
+        serverQueue.connection.dispatcher.end();
+    } else {
+        return message.channel.send("Wrong value!");
+    }
 }
 
 function stop(message, serverQueue) {
@@ -147,7 +191,7 @@ function stop(message, serverQueue) {
     serverQueue.connection.dispatcher.end();
 }
 
-function play(guild, song) {
+async function play(guild, song) {
     const serverQueue = queue.get(guild.id);
     if (!song) {
         serverQueue.voiceChannel.leave();
@@ -164,7 +208,113 @@ function play(guild, song) {
         })
         .on("error", error => console.error(error));
     dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
-    serverQueue.textChannel.send(`Start playing: **${song.title}**`);
+
+    const embed = new MessageEmbed()
+        .setColor("#FD1C03")
+        .setTitle("Playing")
+        .setURL(song.url)
+        .setAuthor(song.author)
+        .setDescription(`**${song.title}**`)
+        .setThumbnail(song.image)
+        .setTimestamp()
+        .setFooter("Copyright © Nixøn 2021", process.env.image);
+    // ø ©
+
+    const { textChannel } = serverQueue;
+
+    textChannel.messages
+        .fetch({ limit: 1 })
+        .then(messages => {
+            let lastMessage = messages.first();
+
+            if (!lastMessage.author.bot) playMsg = false;
+        })
+        .catch(console.error);
+
+    if (!playMsg) {
+        playMsg = await textChannel.send({
+            embed,
+        });
+    } else {
+        playMsg.edit({
+            embed,
+        });
+    }
 }
 
-client.login(token);
+function shuffle(message, serverQueue) {
+    if (!message.member.voice.channel)
+        return message.channel.send("You have to be in a voice channel to shuffle!");
+    if (!serverQueue || serverQueue.songs.length < 2)
+        return message.channel.send("There is not a queue to shuffle!");
+
+    serverQueue.songs = shuffleFunction(serverQueue.songs);
+}
+
+function status(message, serverQueue) {
+    if (!message.member.voice.channel)
+        return message.channel.send({
+            embed: {
+                color: 0x80ff00,
+                description: "You have to be in a voice channel to stop the music!",
+            },
+        });
+
+    if (!serverQueue || !serverQueue.songs)
+        return message.channel.send({
+            embed: { color: 0x80ff00, description: "There are no songs queued at the moment!" },
+        });
+
+    const embed = new MessageEmbed()
+        .setColor("#FD1C03")
+        .setTitle("Current Queue:")
+        // .setDescription(`**${song.title}**`)
+        // .setThumbnail(song.image)
+        .setTimestamp()
+        .setFooter("Copyright © Nixøn 2021", process.env.image);
+
+    if (serverQueue.songs.length > 3) {
+        for (let i = 0; i < 3; i++) {
+            const song = serverQueue.songs[i];
+
+            embed.addField("\u200B", song.title);
+            // embed.addField(song.author, )
+            // embed.addFields([
+            //     { name: "\u200B", value: "\u200B" },
+            //     { name: song.author, value: song.title },
+            // ]);
+        }
+        embed.addField("\u200B", "...");
+    } else {
+        for (const song of serverQueue.songs) {
+            embed.addField("\u200B", song.title);
+        }
+    }
+
+    message.channel.send({
+        embed,
+    });
+}
+
+// Utilities
+function isNumeric(str) {
+    if (typeof str != "string") return false;
+    return !isNaN(str) && !isNaN(parseFloat(str));
+}
+
+function shuffleFunction(array) {
+    let currentIndex = array.length,
+        randomIndex;
+
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // And swap it with the current element.
+        [ array[currentIndex], array[randomIndex] ] = [ array[randomIndex], array[currentIndex] ];
+    }
+
+    return array;
+}
